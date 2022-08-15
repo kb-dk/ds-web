13194500e2799b512b766db876be6d5a8ad0fb34
jenskaj <jkaj@kb.dk>
2022-08-15 11:26:20 +0200

    WOK-378: added comment

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,18 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
+
 /* Stylesheets */
 import './assets/styles/style.scss';
 
 /* Javscript */
+import './assets/js/jquery.js';
+import './assets/js/jquery.dataTables.js';
 import './assets/js/slick.js';
 import './assets/js/choices.js';
 import './assets/js/popper.js';
 import './assets/js/bootstrap.js';
 import './assets/js/bootstrap-select';
-import './assets/js/jquery.dataTables.js';
 import './assets/js/app.js';
 
 /* Third party */
