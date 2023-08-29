4773d36ad1051c1a7bf1bbd3ffd4faf582e5a9cf
Jesper Lauridsen <jela@kb.dk>
2023-08-29 09:39:35 +0200

    first stab, split out into components

---
+++ src/components/nav/Header.vue
@@ -0,0 +1,19 @@
+<template>
+	<div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import './header-component';
+import './desk-component';
+import './mobile-component';
+
+export default defineComponent({
+	name: 'Header',
+	props: {},
+
+	setup() {
+		console.log('header setup');
+	},
+});
+</script>
---
+++ src/components/nav/mobile-component.ts
@@ -0,0 +1,44 @@
+class MobileComponent extends HTMLElement {
+	shadow: ShadowRoot;
+
+	constructor() {
+		super();
+		this.shadow = this.attachShadow({ mode: 'open' });
+		this.shadow.innerHTML = DESKTOP_COMPONENT_TEMPLATE + DESKTOP_COMPONMENT_STYLES;
+
+		/* const observer = new IntersectionObserver((entries) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					this.style.opacity = '1';
+					observer.disconnect();
+				}
+			});
+		});
+
+		observer.observe(this); */
+	}
+
+	static get observedAttributes() {
+		return [''];
+	}
+	attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+		if (name === '') {
+			//console.log("something")
+		}
+	}
+}
+
+const MOBILE_COMPONENT_TEMPLATE = /*html*/ `
+	<div class="container main-12">
+		<div class="row">
+			<div class="col">
+			</div>
+		</div>
+	</div>
+`;
+
+const MOBILE_COMPONMENT_STYLES = /*css*/ `
+	<style>
+	</style>`;
+
+customElements.define('kb-header', MobileComponent);
