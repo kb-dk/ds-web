a1cd2a52d98ee2497832e335f2d9d07d5b7c3efb
jorntx <jt@kb.dk>
2023-09-06 14:44:06 +0200

    Add true global support Only one error-manager exists at a time The instance of error-manager created is available app wide (injection)

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,17 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import { provideErrorManager } from './error-handling/error-manager';
 
 import './assets/fonts/iconfont/material-icons.css';
 
 /* Third party */
 import i18n from './i18n';
 import { createPinia } from 'pinia';
-const pinia = createPinia();
 
-createApp(App).use(router).use(pinia).use(i18n).mount('#app');
+const pinia = createPinia();
+const app = createApp(App).use(router).use(pinia).use(i18n);
+
+//Attach to error manager globally
+provideErrorManager(app);
+app.mount('#app');
