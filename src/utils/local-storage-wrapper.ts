c98adc4284685f5ad5ed078406d52f67ab0a55d7
jorntx <jt@kb.dk>
2023-10-12 22:22:05 +0200

    Add support for persisting locale change Add local storage wrapper Note DISC-303 will complete this feature

--- src/utils/local-storage-wrapper.ts
+++ src/utils/local-storage-wrapper.ts
@@ -1,14 +1,15 @@
 export class LocalStorageWrapper {
-	/* Maybe this is too typescript stringent because 9 out of
-	 10 times this is called we retrieve a string but for now
-	 we will enforce that you have to supply a type just in case */
-	public static get<T>(key: string): T | null {
+	public static get(key: string): string | null {
 		const item = localStorage.getItem(key);
 		if (item) {
+			// First we try for an JSON object
 			try {
-				return JSON.parse(item) as T;
+				return JSON.parse(item);
 			} catch (error) {
-				return item as T;
+				/* If the JSON.parse fails it's probably a plain 
+				string or something else and we just return it
+			 **/
+				return item;
 			}
 		}
 		return null;
