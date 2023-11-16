2518a93bafba190b4977b7cdbdc35d739272a03c
jorntx <jt@kb.dk>
2023-11-16 13:49:46 +0100

    Clean code up and dry it out a little more

--- src/components/search/Facets.vue
+++ src/components/search/Facets.vue
@@ -27,7 +27,7 @@
 import { defineComponent, PropType, onMounted, ref, onUnmounted, watch } from 'vue';
 import { useSearchResultStore } from '@/store/searchResultStore';
 import { FacetResultType } from '@/types/GenericSearchResultTypes';
-import { useRoute, useRouter } from 'vue-router';
+import { useRoute, useRouter, RouteLocationNormalizedLoaded } from 'vue-router';
 
 import '@/components/search/wc-facet-checkbox';
 
@@ -83,19 +83,18 @@ export default defineComponent({
 			return searchResultStore.filters.includes(`fq=${encodeURIComponent(filterString)}`);
 		};
 
-		const updateFilters = (e: CustomEvent) => {
-			const routeQueries = e.detail.add ? addFilter(route, e.detail.filter) : removeFilter(route, e.detail.filter);
-
-			router.push({ query: routeQueries });
+		const cloneRouteQuery = (route: RouteLocationNormalizedLoaded) => {
+			/* Future coder - don't even ask, the line of code below took me hours....
+         I wanted to do it the right way: const routeQueries = { ...route.query };
+         but no worki - instead I had to do this deep clone trickery below.
+         For more info on why this is broken:
+         https://github.com/vuejs/vue-router/issues/1182
+         **/
+			return JSON.parse(JSON.stringify(route.query));
 		};
 
-		const filterUpdateHelper = (e: Event) => {
-			updateFilters(e as CustomEvent);
-			emit('facetUpdate');
-		};
-
-		const addFilter = (route, filter) => {
-			const routeQueries = JSON.parse(JSON.stringify(route.query));
+		const addFilter = (route: RouteLocationNormalizedLoaded, filter: string) => {
+			const routeQueries = cloneRouteQuery(route);
 			const newFilter = encodeURIComponent(filter);
 
 			if (!routeQueries.fq) {
@@ -106,55 +105,31 @@ export default defineComponent({
 				// This will only trigger if someone manipulates the url manually
 				routeQueries.fq = [routeQueries.fq, newFilter];
 			}
-
 			return routeQueries;
 		};
 
-		const removeFilter = (route, filter) => {
-			const routeQueries = JSON.parse(JSON.stringify(route.query));
+		const removeFilter = (route: RouteLocationNormalizedLoaded, filter: string) => {
+			const routeQueries = cloneRouteQuery(route);
 			const filterToRemove = encodeURIComponent(filter);
 
 			if (Array.isArray(routeQueries.fq)) {
-				routeQueries.fq = routeQueries.fq.filter((item) => item !== filterToRemove);
+				routeQueries.fq = routeQueries.fq.filter((item: string) => item !== filterToRemove);
 			} else {
 				routeQueries.fq = undefined;
 			}
-
 			return routeQueries;
 		};
 
-		//const updateFilters = (e: CustomEvent) => {
-		/* Future coder - don't even ask, the line of code below took me hours....
-				 I wanted to do it the right way: const routeQueries = { ...this.$route.query };
-				 but no worki - instead I had to do this copy trickery below.
-				 For more info on why this is broken:
-				 https://github.com/vuejs/vue-router/issues/1182
-				 **/
-		//TODO the logic below could be split up into a couple of methods.
-		//Lets do that when we know the functionality is solid and we refactor
-		//the whole component to composition API
-		/*const routeQueries = JSON.parse(JSON.stringify(route.query));
-			if (e.detail.add) {
-				const newFilter = encodeURIComponent(e.detail.filter);
-				if (!routeQueries.fq) {
-					routeQueries.fq = [newFilter];
-				} else if (Array.isArray(routeQueries.fq)) {
-					routeQueries.fq.push(newFilter);
-				} else {
-					//This will only trigger if someone manipulates the url manually
-					routeQueries.fq = [routeQueries.fq, newFilter];
-				}
-			} else {
-				// if there's more than one, it's an array. If there's just one, it a string (apparently), that we can just remove.
-				const filterToRemove = encodeURIComponent(e.detail.filter);
-				if (Array.isArray(routeQueries.fq)) {
-					routeQueries.fq = routeQueries.fq.filter((item: string) => item !== filterToRemove);
-				} else {
-					routeQueries.fq = undefined;
-				}
-			}
+		const updateFilters = (e: CustomEvent) => {
+			const routeQueries = e.detail.add ? addFilter(route, e.detail.filter) : removeFilter(route, e.detail.filter);
 			router.push({ query: routeQueries });
-		};*/
+		};
+
+		const filterUpdateHelper = (e: Event) => {
+			updateFilters(e as CustomEvent);
+			emit('facetUpdate');
+		};
+
 		// A simple method to arrange the facets in an orderly fasion, so they're easier to loop through.
 		// Might not be relevant when we know more about the backend structure.
 		const simplifyFacets = (facet: Array<string>) => {
