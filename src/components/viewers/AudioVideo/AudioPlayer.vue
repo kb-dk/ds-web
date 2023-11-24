4032d43c61a7092fb4953dc81229835b0b954fee
jorntx <jt@kb.dk>
2023-11-24 11:40:21 +0100

    Fix for styles not imported correctly

--- src/components/viewers/AudioVideo/AudioPlayer.vue
+++ src/components/viewers/AudioVideo/AudioPlayer.vue
@@ -1,22 +1,30 @@
 <template>
-	<media-player
-		class="player"
-		title=""
-		:src="src"
-		ref="player"
-		view-type="audio"
+	<div class="mobile-edge edge top"></div>
+	<div
+		class="audio-player"
+		v-if="src"
 	>
-		<media-provider></media-provider>
-	</media-player>
+		<media-player
+			class="player"
+			title=""
+			:src="src"
+			ref="player"
+			view-type="audio"
+		>
+			<media-provider />
+			<media-audio-layout />
+		</media-player>
+	</div>
+	<div class="mobile-edge edge bottom"></div>
 </template>
 
 <script lang="ts">
 import { ref, onMounted, onBeforeUnmount, defineComponent } from 'vue';
 import type { MediaPlayerElement } from 'vidstack/elements';
 
-import 'vidstack/player/styles/base.css';
-
 import 'vidstack/player';
+import 'vidstack/player/layouts';
+import 'vidstack/player/ui';
 
 export default defineComponent({
 	name: 'AudioPlayer',
@@ -29,13 +37,11 @@ export default defineComponent({
 		const src = ref('');
 		onMounted(() => {
 			src.value = props.audioUrl ? props.audioUrl : '';
-			// Clean up
 			const cleanup = () => {
 				if (player.value) {
 					player.value.destroy();
 				}
 			};
-
 			// Attach cleanup to unmmount
 			onBeforeUnmount(() => {
 				cleanup();
@@ -46,11 +52,62 @@ export default defineComponent({
 });
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only 
-temporary styling until patterns from design system are implemented 
--->
 <style scoped>
-.player {
-	display: block;
+@import 'vidstack/player/styles/default/theme.css';
+@import 'vidstack/player/styles/default/layouts/audio.css';
+
+.audio-player {
+	background-color: black;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	margin-left: 0px;
+	overflow-y: hidden;
+	padding-top: 31px;
+	padding-bottom: 31px;
+	background-color: white;
+}
+
+.edge {
+	height: 31px;
+}
+
+.mobile-edge {
+	width: 100%;
+	position: absolute;
+	background-color: white;
+	clip-path: polygon(100% 0, 0 0, 0 100%);
+	z-index: 3;
+	left: 0px;
+}
+
+.top {
+	clip-path: polygon(100% 0, 0 0, 0 100%);
+}
+
+.bottom {
+	clip-path: polygon(100% 0, 100% 100%, 0 100%);
+	margin-top: -30px;
+}
+
+@media (min-width: 640px) {
+	.video-player {
+		margin-left: -36px;
+		width: 100vw;
+		max-width: calc(100% + 36px * 2);
+	}
+}
+
+@media (min-width: 990px) {
+	.mobile-edge {
+		display: none;
+	}
+	.video-player {
+		width: 100%;
+		margin-left: 0px;
+		max-width: 100%;
+		padding: 0px;
+	}
 }
 </style>
