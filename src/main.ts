83260c67b323ffe7075e379ff77eecb22d36b3fc
Jesper Lauridsen <jela@kb.dk>
2024-03-14 09:53:05 +0100

    decent start

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import router from './router';
 import { provideErrorManager } from '@/components/global/error-handling/error-manager';
 
 import './assets/fonts/iconfont/material-icons.css';
+import 'https://cookie-script.com/s/903181bb69e77c4a5adfc4ea71d034aa.js';
 
 /* Third party */
 import i18n from './i18n';
