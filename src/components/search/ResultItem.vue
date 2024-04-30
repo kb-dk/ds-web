934f3fbb8aaf148e3e6e233514b1d41522f59642
Jesper Lauridsen <jela@kb.dk>
2024-04-30 13:39:32 +0200

    shadow adjustment

--- src/components/search/ResultItem.vue
+++ src/components/search/ResultItem.vue
@@ -423,11 +423,11 @@ export default defineComponent({
 		z-index: 0;
 		opacity: 0;
 		box-shadow:
-			rgba(0, 0, 0, 0.09) 0px 2px 1px,
-			rgba(0, 0, 0, 0.09) 0px 4px 2px,
-			rgba(0, 0, 0, 0.09) 0px 8px 4px,
-			rgba(0, 0, 0, 0.09) 0px 16px 8px,
-			rgba(0, 0, 0, 0.09) 0px 32px 16px;
+			rgba(0, 0, 0, 0.05) 0px 2px 1px,
+			rgba(0, 0, 0, 0.04) 0px 4px 2px,
+			rgba(0, 0, 0, 0.03) 0px 8px 4px,
+			rgba(0, 0, 0, 0.02) 0px 16px 8px,
+			rgba(0, 0, 0, 0.01) 0px 32px 16px;
 		position: absolute;
 		top: 0px;
 		left: 0px;
