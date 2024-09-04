8e0216843ca6828f5228b352c8c80f00e62d73e0
jorntx <jt@kb.dk>
2024-09-04 12:32:22 +0200

    Handle 401 scenario and and auto refresh session
    - handle 401 in api-client
    - add global service retry logic in api-client if service calls encounter 401
    - retry 2 times if 401 occours and auth call fails
    - set session to 36 hours to match backend
    - refresh session 15 minutes before session espires

--- src/api/api-client.ts
+++ src/api/api-client.ts
@@ -1,4 +1,5 @@
-import { AxiosInstance, AxiosResponse, AxiosError } from 'axios';
+import { AxiosInstance, AxiosResponse, AxiosError, AxiosRequestConfig } from 'axios';
+import { useAuthStore } from '@/store/authStore';
 import {
 	APISearchResponseType,
 	APIRecordResponseType,
@@ -8,15 +9,16 @@ import {
 	APIAuthResponseType,
 } from '@/types/APIResponseTypes';
 
-export function sleep(random?: boolean): Promise<void> {
-	let sleep = 2000;
-	if (random) {
-		sleep = Math.random() * 1500 + 500;
-	}
-	return new Promise((resolve) => setTimeout(resolve, sleep));
+// Generic sleep function used for both 'authenticate retries' and 'articial latency in dev environment'
+export function sleep(duration: number): Promise<void> {
+	return new Promise((resolve) => setTimeout(resolve, duration));
 }
 
 export class APIServiceClient {
+	private pendingRequests: (() => Promise<unknown>)[] = [];
+	private maxRetries = 2;
+	private retryDelay = 2000;
+
 	constructor(private httpClient: AxiosInstance) {
 		httpClient.interceptors.request.use(
 			(config) => {
@@ -38,17 +40,72 @@ export class APIServiceClient {
 						response.config.url?.includes('mlt') ||
 						response.config.url?.includes('thumbnails');
 					if (!noDelayRequest) {
-						await sleep(true);
+						await sleep(2000);
 					}
 				}
 				return response;
 			},
-			(error: AxiosError) => {
+			// This turned out to be a somewhat complex error handler but hopefully the comments will help
+			async (error: AxiosError) => {
+				// If server throws 401 we know something is up with the users authorization and we try to help
+				if (error.response?.status === 401) {
+					const authStore = useAuthStore();
+					const config = error.config;
+
+					// I threw this check in just in case but it shouldn't happen
+					if (!config) {
+						return Promise.reject(error);
+					}
+
+					// We store the original request so we can use it later
+					const originalRequest = () => this.httpClient.request(config as AxiosRequestConfig);
+
+					if (authStore.isAuthenticating) {
+						// If authentication is already in progress, queue the request
+						return new Promise((resolve, reject) => {
+							this.pendingRequests.push(() => originalRequest().then(resolve).catch(reject));
+						});
+					}
+
+					try {
+						// Try to authenticate
+						await this.handleAuthenticationWithRetry(authStore);
+
+						/** A bit complex here but what is happening is in short:
+						 *  Retry all pending requests after successful authentication (thats all
+						 * 	the map((req) => req() business) and subsequently clear the queue.
+						 *  After that we give the original request a go.
+						 * 	If the auth call fails, we clear the pending requests queue and reject
+						 *  with an error.
+						 */
+						await Promise.all(this.pendingRequests.map((req) => req()));
+						this.pendingRequests = [];
+						return originalRequest();
+					} catch (authError) {
+						this.pendingRequests = [];
+						return Promise.reject(authError);
+					}
+				}
 				return Promise.reject(error);
 			},
 		);
 	}
 
+	//Private method to handle authentication and retries if the service fails
+	private async handleAuthenticationWithRetry(authStore: ReturnType<typeof useAuthStore>) {
+		for (let attempt = 0; attempt <= this.maxRetries; attempt++) {
+			try {
+				await authStore.authenticate();
+				return; // Exit if authentication is successful
+			} catch (error) {
+				if (attempt < this.maxRetries) {
+					await sleep(this.retryDelay); // Wait before retrying via the generic sleep method
+				} else {
+					throw error; // Throw error if all retries fail and we are dead in the water
+				}
+			}
+		}
+	}
 	//Search and record methods
 	async getFacetResults(query: string, filters: string, start: string, sort: string): Promise<APISearchResponseType> {
 		return await this.httpClient.get(
