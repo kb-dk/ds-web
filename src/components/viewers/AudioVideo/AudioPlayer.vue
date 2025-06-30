26731ceca98a20161e47b28b5593c2b2bb4debff
Jesper Lauridsen <jela@kb.dk>
2025-06-30 09:36:55 +0200

    Added images for audio player sound

--- src/components/viewers/AudioVideo/AudioPlayer.vue
+++ src/components/viewers/AudioVideo/AudioPlayer.vue
@@ -15,7 +15,19 @@ import { KalturaPlayerType, PlayerType } from '@/types/KalturaTypes';
 import { useAuthStore } from '@/store/authStore';
 import { useRoute } from 'vue-router';
 import { Priority, Severity } from '@/types/NotificationType';
-import startPicture from '@/assets/dr_lyd.jpg';
+import boogie from '@/assets/images/audio-thumbnails/boogie.jpg';
+import initial from '@/assets/images/audio-thumbnails/default.jpg';
+import mama from '@/assets/images/audio-thumbnails/mama.jpg';
+import p1 from '@/assets/images/audio-thumbnails/p1.jpg';
+import p2 from '@/assets/images/audio-thumbnails/p2.jpg';
+import p3 from '@/assets/images/audio-thumbnails/p3.jpg';
+import p4 from '@/assets/images/audio-thumbnails/p4.jpg';
+import p5 from '@/assets/images/audio-thumbnails/p5.jpg';
+import p6 from '@/assets/images/audio-thumbnails/p6.jpg';
+import p7 from '@/assets/images/audio-thumbnails/p7.jpg';
+import p8 from '@/assets/images/audio-thumbnails/p8.jpg';
+import p5000 from '@/assets/images/audio-thumbnails/p5000.jpg';
+import ramasjang from '@/assets/images/audio-thumbnails/ramasjang.jpg';
 
 // Third party script - global variable typing and declaring.
 declare const KalturaPlayer: KalturaPlayerType;
@@ -89,8 +101,47 @@ export default defineComponent({
 		};
 
 		const getThumbnailPicture = (channel: string) => {
-			console.log('we got picture!', channel);
-			return startPicture;
+			channel = channel.toLowerCase();
+			switch (true) {
+				case channel.includes('p5000'):
+					return p5000;
+
+				case channel.includes('mama'):
+					return mama;
+
+				case channel.includes('boogie'):
+					return boogie;
+
+				case channel.includes('ramasjang'):
+					return ramasjang;
+
+				case channel.includes('p1'):
+					return p1;
+
+				case channel.includes('p2'):
+					return p2;
+
+				case channel.includes('p3'):
+					return p3;
+
+				case channel.includes('p4'):
+					return p4;
+
+				case channel.includes('p5'):
+					return p5;
+
+				case channel.includes('p6'):
+					return p6;
+
+				case channel.includes('p7'):
+					return p7;
+
+				case channel.includes('p8'):
+					return p8;
+
+				default:
+					return initial;
+			}
 		};
 
 		const appendScript = () => {
