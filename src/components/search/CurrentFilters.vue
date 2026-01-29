558da448f35c2395d12fc032d41268935781b927
pbruun <patrickbj95@hotmail.com>
2026-01-29 13:04:44 +0100

    facetsOpen set to true. So date filter isn't removed when choosing another filter. Scroll to top, if reset filters.

--- src/components/search/CurrentFilters.vue
+++ src/components/search/CurrentFilters.vue
@@ -250,7 +250,7 @@ export default defineComponent({
 			delete routeQueries.start;
 			delete routeQueries.fq;
 			delete routeQueries.sort;
-
+			window.scrollTo({ top: 0, behavior: 'smooth' });
 			router.push({
 				name: 'Search',
 				query: routeQueries,
