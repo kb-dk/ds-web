fc0d46a6a54e60afcb27115915a5127adc2bc047
pbruun <patrickbj95@hotmail.com>
2026-07-09 11:28:08 +0200

    Able to use slider on the edges for mobile

--- src/components/common/timeSearch/TimeSearchFilters.vue
+++ src/components/common/timeSearch/TimeSearchFilters.vue
@@ -568,6 +568,8 @@ fieldset {
 .slider-whiteoff-container {
 	position: relative;
 	margin-bottom: 20px;
+	padding-left: 15px;
+	padding-right: 15px;
 }
 .explanation-for-data {
 	position: absolute;
@@ -952,6 +954,8 @@ fieldset {
 	top: 0px;
 	height: 0px;
 	visibility: hidden;
+	padding-left: 15px;
+	padding-right: 15px;
 }
 /* MEDIA QUERY 990 */
 @media (min-width: 1200px) {
@@ -999,6 +1003,14 @@ fieldset {
 
 	.explanation-for-data {
 		display: flex;
+	}
+}
+@media (min-width: 1320px) {
+	.slider-whiteoff-container {
+		padding: 0;
+	}
+	.data-container {
+		padding: 0;
 	}
 }
 </style>
