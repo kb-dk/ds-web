e1d1bb4d8d5c28247f16fbda3bc24fb8640b396a
Jesper Lauridsen <jela@kb.dk>
2026-07-10 09:48:27 +0200

    small fix

--- src/components/global/Logo.vue
+++ src/components/global/Logo.vue
@@ -173,74 +173,74 @@ export default defineComponent({
 }
 
 @media (max-width: 990px) {
-	.line:nth-of-type(1) {
+	.line.animate:nth-of-type(1) {
 		top: 44.07px;
 	}
-	.line:nth-of-type(2) {
+	.line.animate:nth-of-type(2) {
 		top: 154.43px;
 	}
-	.line:nth-of-type(3) {
+	.line.animate:nth-of-type(3) {
 		opacity: 0.7 !important;
 		top: 139px;
 	}
-	.line:nth-of-type(4) {
+	.line.animate:nth-of-type(4) {
 		opacity: 0.2 !important;
 		top: 197px;
 	}
-	.line:nth-of-type(5) {
+	.line.animate:nth-of-type(5) {
 		top: 305px;
 	}
 }
 
 @media (min-width: 990px) and (max-width: 1279px) {
-	.line:nth-of-type(1) {
+	.line.animate:nth-of-type(1) {
 		top: 44.07px;
 	}
-	.line:nth-of-type(2) {
+	.line.animate:nth-of-type(2) {
 		top: 154.43px;
 	}
-	.line:nth-of-type(3) {
+	.line.animate:nth-of-type(3) {
 		top: 339px;
 	}
-	.line:nth-of-type(4) {
+	.line.animate:nth-of-type(4) {
 		top: 497px;
 	}
-	.line:nth-of-type(5) {
+	.line.animate:nth-of-type(5) {
 		top: 698px;
 	}
 }
 
 @media (min-width: 1280px) and (max-width: 1439px) {
-	.line:nth-of-type(1) {
+	.line.animate:nth-of-type(1) {
 		top: 44.07px;
 	}
-	.line:nth-of-type(2) {
+	.line.animate:nth-of-type(2) {
 		top: 154.43px;
 	}
-	.line:nth-of-type(3) {
+	.line.animate:nth-of-type(3) {
 		top: 355px;
 	}
-	.line:nth-of-type(4) {
+	.line.animate:nth-of-type(4) {
 		top: 615px;
 	}
-	.line:nth-of-type(5) {
+	.line.animate:nth-of-type(5) {
 		top: 815px;
 	}
 }
 @media (min-width: 1440px) {
-	.line:nth-of-type(1) {
+	.line.animate:nth-of-type(1) {
 		top: 44.07px;
 	}
-	.line:nth-of-type(2) {
+	.line.animate:nth-of-type(2) {
 		top: 154.43px;
 	}
-	.line:nth-of-type(3) {
+	.line.animate:nth-of-type(3) {
 		top: 427px;
 	}
-	.line:nth-of-type(4) {
+	.line.animate:nth-of-type(4) {
 		top: 730px;
 	}
-	.line:nth-of-type(5) {
+	.line.animate:nth-of-type(5) {
 		top: 933px;
 	}
 }
